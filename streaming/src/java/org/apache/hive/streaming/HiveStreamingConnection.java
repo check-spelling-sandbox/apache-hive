999c6ae7698fb5493b23a5d66779c2bf850d5bab
Josh Soref <jsoref@users.noreply.github.com>
2020-11-12 18:52:20 -0500

    spelling: heartbeat
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- streaming/src/java/org/apache/hive/streaming/HiveStreamingConnection.java
+++ streaming/src/java/org/apache/hive/streaming/HiveStreamingConnection.java
@@ -494,7 +494,7 @@ public class HiveStreamingConnection implements StreamingConnection {
     return msClient;
   }
 
-  IMetaStoreClient getHeatbeatMSC() {
+  IMetaStoreClient getHeartbeatMSC() {
     connectionStats.incrementMetastoreCalls();
     return heartbeatMSClient;
   }
@@ -687,7 +687,7 @@ public class HiveStreamingConnection implements StreamingConnection {
     } finally {
       if (manageTransactions) {
         getMSC().close();
-        getHeatbeatMSC().close();
+        getHeartbeatMSC().close();
         try {
           // Close the HMS that is used for addWriteNotificationLog
           Hive.get(conf).getSynchronizedMSC().close();
