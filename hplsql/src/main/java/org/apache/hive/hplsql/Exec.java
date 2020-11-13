281ef513911c0a34ffa2b90c9d2971e4b21afd81
Josh Soref <jsoref@users.noreply.github.com>
2020-11-12 20:26:57 -0500

    spelling: retrieve
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- hplsql/src/main/java/org/apache/hive/hplsql/Exec.java
+++ hplsql/src/main/java/org/apache/hive/hplsql/Exec.java
@@ -2781,7 +2781,7 @@ public class Exec extends HplsqlBaseVisitor<Integer> implements Closeable {
   }
   
   /**
-   * Trace values retrived from the database
+   * Trace values retrieved from the database
    */
   public void trace(ParserRuleContext ctx, Var var, Metadata meta, int idx) {
     if (var.type != Var.Type.ROW) {
