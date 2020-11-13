fb6e5e7777d45a0568f0aaf2f18f59cb11ea5be8
Josh Soref <jsoref@users.noreply.github.com>
2020-11-12 20:24:30 -0500

    spelling: reliable
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- ql/src/java/org/apache/hadoop/hive/ql/stats/BasicStatsTask.java
+++ ql/src/java/org/apache/hadoop/hive/ql/stats/BasicStatsTask.java
@@ -385,7 +385,7 @@ public class BasicStatsTask implements Serializable, IStatsProcessor {
         }
 
         if (work.isStatsReliable() && updates.size() != processors.size()) {
-          LOG.info("Stats should be reliadble...however seems like there were some issue.. => ret 1");
+          LOG.info("Stats should be reliable...however seems like there were some issue.. => ret 1");
           ret = 1;
         }
       }
