d3cecfb65ad4b069e356494c3a7834692fb457da
Josh Soref <jsoref@users.noreply.github.com>
2020-11-12 20:25:52 -0500

    spelling: requires
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- ql/src/test/org/apache/hadoop/hive/ql/TestTxnCommands2.java
+++ ql/src/test/org/apache/hadoop/hive/ql/TestTxnCommands2.java
@@ -879,7 +879,7 @@ public class TestTxnCommands2 extends TxnCommandsBaseForTests {
   }
 
   /**
-   * Test update that hits multiple partitions (i.e. requries dynamic partition insert to process)
+   * Test update that hits multiple partitions (i.e. requires dynamic partition insert to process)
    * @throws Exception
    */
   @Test
