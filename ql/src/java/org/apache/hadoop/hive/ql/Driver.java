0d3888c43029758789f6abc27e0d765eee14dc92
Josh Soref <jsoref@users.noreply.github.com>
2020-11-13 12:26:09 -0500

    spelling: destroyed
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- ql/src/java/org/apache/hadoop/hive/ql/Driver.java
+++ ql/src/java/org/apache/hadoop/hive/ql/Driver.java
@@ -880,7 +880,7 @@ public class Driver implements IDriver {
       if (driverState.isDestroyed()) {
         return;
       } else {
-        driverState.descroyed();
+        driverState.destroyed();
       }
     } finally {
       driverState.unlock();
