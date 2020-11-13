40e27b2c71edf7e808be2ddb29662fba1a229c89
Josh Soref <jsoref@users.noreply.github.com>
2020-11-12 19:58:09 -0500

    spelling: persistence
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- standalone-metastore/metastore-server/src/test/java/org/apache/hadoop/hive/metastore/TestHiveMetaStore.java
+++ standalone-metastore/metastore-server/src/test/java/org/apache/hadoop/hive/metastore/TestHiveMetaStore.java
@@ -3301,17 +3301,17 @@ public abstract class TestHiveMetaStore {
   }
 
   @Test
-  public void testJDOPersistanceManagerCleanup() throws Exception {
+  public void testJDOPersistenceManagerCleanup() throws Exception {
     if (isThriftClient == false) {
       return;
     }
 
-    int numObjectsBeforeClose =  getJDOPersistanceManagerCacheSize();
+    int numObjectsBeforeClose =  getJDOPersistenceManagerCacheSize();
     HiveMetaStoreClient closingClient = new HiveMetaStoreClient(conf);
     closingClient.getAllDatabases();
     closingClient.close();
     Thread.sleep(5 * 1000); // give HMS time to handle close request
-    int numObjectsAfterClose =  getJDOPersistanceManagerCacheSize();
+    int numObjectsAfterClose =  getJDOPersistenceManagerCacheSize();
     assertTrue(numObjectsBeforeClose == numObjectsAfterClose);
 
     HiveMetaStoreClient nonClosingClient = new HiveMetaStoreClient(conf);
@@ -3320,11 +3320,11 @@ public abstract class TestHiveMetaStore {
     // will trigger cleanup
     nonClosingClient.getTTransport().close();
     Thread.sleep(5 * 1000);
-    int numObjectsAfterDroppedConnection =  getJDOPersistanceManagerCacheSize();
+    int numObjectsAfterDroppedConnection =  getJDOPersistenceManagerCacheSize();
     assertTrue(numObjectsAfterClose == numObjectsAfterDroppedConnection);
   }
 
-  private static int getJDOPersistanceManagerCacheSize() {
+  private static int getJDOPersistenceManagerCacheSize() {
     JDOPersistenceManagerFactory jdoPmf;
     Set<JDOPersistenceManager> pmCacheObj;
     Field pmCache;
