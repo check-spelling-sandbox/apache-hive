144d18f5480ca517caddcd514baa6b941816fc94
Josh Soref <jsoref@users.noreply.github.com>
2020-11-12 20:29:50 -0500

    spelling: schedule
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- llap-tez/src/test/org/apache/hadoop/hive/llap/tezplugins/TestLlapTaskSchedulerService.java
+++ llap-tez/src/test/org/apache/hadoop/hive/llap/tezplugins/TestLlapTaskSchedulerService.java
@@ -2286,7 +2286,7 @@ public class TestLlapTaskSchedulerService {
 
     @Override
     protected void schedulePendingTasks() throws InterruptedException {
-      LOG.info("Attempted schedulPendingTasks");
+      LOG.info("Attempted schedulePendingTasks");
       testLock.lock();
       try {
         if (controlScheduling.get()) {
