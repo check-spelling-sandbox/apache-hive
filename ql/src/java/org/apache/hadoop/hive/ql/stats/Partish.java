jsoref/hive

spelling: partition
Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

ql/src/java/org/apache/hadoop/hive/ql/stats/Partish.java
@@ -141,9 +141,9 @@ public abstract class Partish {
     private Partition partition;
 
     // FIXME: possibly the distinction between table/partition is not need; however it was like this before....will change it later
-    public PPart(Table table, Partition partiton) {
+    public PPart(Table table, Partition partition) {
       this.table = table;
-      partition = partiton;
+      partition = partition;
     }
 
     @Override

check-spelling-sandbox/apache-hive

ql/src/java/org/apache/hadoop/hive/ql/stats/Partish.java
@@ -141,9 +141,9 @@ public abstract class Partish {
     private Partition partition;
 
     // FIXME: possibly the distinction between table/partition is not need; however it was like this before....will change it later
-    public PPart(Table table, Partition partiton) {
+    public PPart(Table table, Partition part) {
       this.table = table;
-      partition = partiton;
+      partition = part;
     }
 
     @Override