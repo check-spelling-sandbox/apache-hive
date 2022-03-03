jsoref/hive

spelling: collectors
Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

ql/src/java/org/apache/hadoop/hive/ql/stats/BasicStatsNoJobTask.java
@@ -327,17 +327,17 @@ public class BasicStatsNoJobTask implements IStatsProcessor {
         }
       }
     }
-    List<FooterStatCollector> validColectors = Lists.newArrayList();
+    List<FooterStatCollector> validCollectors = Lists.newArrayList();
     for (FooterStatCollector statsCollection : scs) {
       if (statsCollection.isValid()) {
-        validColectors.add(statsCollection);
+        validCollectors.add(statsCollection);
       }
     }
 
     EnvironmentContext environmentContext = new EnvironmentContext();
     environmentContext.putToProperties(StatsSetupConst.DO_NOT_UPDATE_STATS, StatsSetupConst.TRUE);
 
-    ImmutableListMultimap<String, FooterStatCollector> collectorsByTable = Multimaps.index(validColectors, FooterStatCollector.SIMPLE_NAME_FUNCTION);
+    ImmutableListMultimap<String, FooterStatCollector> collectorsByTable = Multimaps.index(validCollectors, FooterStatCollector.SIMPLE_NAME_FUNCTION);
 
     LOG.debug("Collectors.size(): {}", collectorsByTable.keySet());
 

check-spelling-sandbox/apache-hive

ql/src/java/org/apache/hadoop/hive/ql/stats/BasicStatsNoJobTask.java
@@ -399,17 +399,17 @@ public class BasicStatsNoJobTask implements IStatsProcessor {
         }
       }
     }
-    List<StatCollector> validColectors = Lists.newArrayList();
+    List<StatCollector> validCollectors = Lists.newArrayList();
     for (StatCollector statsCollection : scs) {
       if (statsCollection.isValid()) {
-        validColectors.add(statsCollection);
+        validCollectors.add(statsCollection);
       }
     }
 
     EnvironmentContext environmentContext = new EnvironmentContext();
     environmentContext.putToProperties(StatsSetupConst.DO_NOT_UPDATE_STATS, StatsSetupConst.TRUE);
 
-    ImmutableListMultimap<String, StatCollector> collectorsByTable = Multimaps.index(validColectors, FooterStatCollector.SIMPLE_NAME_FUNCTION);
+    ImmutableListMultimap<String, StatCollector> collectorsByTable = Multimaps.index(validCollectors, FooterStatCollector.SIMPLE_NAME_FUNCTION);
 
     LOG.debug("Collectors.size(): {}", collectorsByTable.keySet());
 